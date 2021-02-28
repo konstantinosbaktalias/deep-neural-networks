27ae1b2981ded3998654c33950bace090dc48f55
konstantinosbaktalias <kostis338@gmail.com>
2021-02-28 21:23:27 +0200

    first commit

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,93 +1,161 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sigmoid(z):
-    return 1 / (1+np.exp(-z))
+# Activation functions
+def sigmoid(z, alpha=1):
+    return (1 / (1+np.exp(-z))) * alpha
+
+def relu(z, alpha=1):
+    z[z < 0] = 0. 
+    return z * alpha
+
+def leaky_relu(z, alpha=1):
+    z = np.where(z > 0, z, z * 0.01) 
+    return z * alpha
+
+def tanh(z, alpha=1):
+    return (np.tanh(z)) * alpha
+
+def softmax(z, alpha=1):
+    return (np.exp(z) / np.sum(np.exp(z))) * alpha
+
+# Derivatives of activations functions
+def sigmoid_prime(a, alpha=1):
+    return (a * (1. - a)) *  alpha
+
+def relu_prime(a, alpha=1):
+    a[a > 0] = 1. 
+    return a * alpha
+
+def leaky_relu_prime(a, alpha=1):
+    a[a > 0] = 1.
+    a = np.where(a > 0, a, 0.01)
+    return a * alpha
+
+def tahn_prime(a, alpha=1):
+    return (1 - a**2) * alpha
+
+def softmax_prime(a, alpha=1):
+    s = a.reshape(-1, 1)
+    j = (np.diagflat(s) - np.dot(s, s.T)).sum(axis=1)
+    return j.reshape(j.shape[0], 1) * alpha
+
+# Loss functions
+def cross_entropy_loss(pred):
+    return -np.log(pred)
 
 def mse_loss(y_true, pred):
     return np.square(y_true - pred).mean()
 
 class NeuralNetwork:
-    def __init__(self, params):
-        self.W = self.init_w(params)
-        self.b = self.init_b(params)
+    def __init__(self, params: list):
+        self.params = params
 
-    def init_w(self, params):
-        weights = []
-        for i in range(1, len(params)):
-            weights.append(np.random.randn(params[i], params[i-1]))
-        return np.array(weights)
+        self.weights: list = []
+        self.biases: list = []
 
-    def init_b(self, params):
-        biases = []
-        for i in range(1, len(params)):
-            biases.append(np.random.randn(params[i]))
-        return np.array(biases)
+        self.initialize_params()
 
-    # Feed forward
-    def forward_prop(self, input):
-        outputs = []
-        z = np.dot(self.W[0], input) + self.b[0]
-        a = sigmoid(z)
-        outputs.append(a)
-        for i in range(1, len(self.W)):
-            z = np.dot(self.W[i], outputs[i-1]) + self.b[i]
-            a = sigmoid(z)
-            outputs.append(a)
+    def initialize_params(self):
+        for layer_param in self.params:
+            # Initialize layer's weights
+            self.weights.append(
+                np.random.randn(layer_param['neurons_size'], layer_param['input_size']) 
+            )
+            # Initialize layer's biases
+            self.biases.append(np.zeros((layer_param['neurons_size'], 1)))
 
-        outputs = np.array(outputs)
-        return outputs[-1]
+    def forward_propagation(self, input, back_prop=False):
+        outputs: list = []
+        
+        for i in range(len(self.params)):
+            # Getting the layer's input 
+            input = outputs[i-1] if len(outputs) > 0 else input
+           
+            if self.params[i]['activation'] == 'sigmoid':
+                activation_func = sigmoid
+            elif self.params[i]['activation'] == 'relu':
+                activation_func = relu
+            elif self.params[i]['activation'] == 'leaky_relu':
+                activation_func = leaky_relu
+            elif self.params[i]['activation'] == 'tanh':
+                activation_func = tanh
+            elif self.params[i]['activation'] == 'softmax':
+                activation_func = softmax
+            else:
+                raise Exception(f'Invalid activation function on layer {i+1}!')
 
-    def back_prop(self, X, Y, epochs, rate, plot=False):
-        costs_arr = []
+            z = self.weights[i] @ input + self.biases[i]
+            outputs.append(activation_func(z, 1 if 'activation_alpha' not in self.params[i] else self.params[i]['activation_alpha']))
+
+        if back_prop:
+            return outputs
+        else:
+            return outputs[-1]
+
+    def back_prop(self, X, Y, epochs, learn_rate, loss_func, plot=True):
+        total_losses: list = []
         for epoch in range(epochs):
-            costs = []
-            # Looping throw the dataset
-            for x_input, y_true in zip(X, Y):
-                    # Feed forward
-                    outputs = []
-                    z = np.dot(self.W[0], x_input) + self.b[0]
-                    a = sigmoid(z)
-                    outputs.append(a)
-                    for i in range(1, len(self.W)):
-                        z = np.dot(self.W[i], outputs[i-1]) + self.b[i]
-                        a = sigmoid(z)
-                        outputs.append(a)
+            epoch_losses: float = 0
+            for input, y_true in zip(X, Y):
+                outputs = self.forward_propagation(input, back_prop=True)
 
-                    outputs = np.array(outputs)
+                target = np.argmax(y_true)
+                pred = outputs[-1].copy()
 
-                    # Get the cost of the prediction
-                    cost =  y_true - outputs[-1]
-                    costs.append(cost)
+                if loss_func == 'MeanSqueredError':
+                    epoch_losses += mse_loss(y_true, outputs[-1])
+                elif loss_func == 'CrossEntropyLoss':
+                    epoch_losses += cross_entropy_loss(pred[target])
+                else:
+                    raise Exception('Invalid loss funtion!')
 
-                    # Adjasting the weights and biases for higher accuracy
-                    for i in reversed(range(1, len(self.W))):
-                        d = (cost * outputs[i] * (1.0 - outputs[i]))
-                        self.W[i] += np.dot(d.reshape(d.shape[0], 1), np.transpose(outputs[i-1].reshape(outputs[i-1].shape[0], 1))) * rate
-                        self.b[i] += cost * outputs[i] * (1.0 - outputs[i]) * rate
-                        cost = np.dot(self.W[i].T, cost)
-                    
-                    d = (cost * outputs[0] * (1.0 - outputs[0]))
-                    self.W[0] += np.dot(d.reshape(d.shape[0], 1), np.transpose(x_input.reshape(x_input.shape[0], 1))) * rate
-                    self.b[0] += cost * outputs[0] * (1.0 - outputs[0]) * rate
+                error = pred
+                error[target] -= 1.
 
-            costs_arr.append(mse_loss(y_true, outputs[-1]))    
-            print(f'Epoch: {epoch+1} | Loss: {mse_loss(y_true, outputs[-1])}')    
+                for i in reversed(range(len(self.params))):
+                    if self.params[i]['activation'] == 'sigmoid':
+                        activation_der = sigmoid_prime
+                    elif self.params[i]['activation'] == 'relu':
+                        activation_der = relu_prime
+                    elif self.params[i]['activation'] == 'leaky_relu':
+                        activation_der = leaky_relu_prime
+                    elif self.params[i]['activation'] == 'tahn':
+                        activation_der = tahn_prime
+                    elif self.params[i]['activation'] == 'softmax':
+                        activation_der = softmax_prime
+
+                    grad = error * activation_der(outputs[i], 1 if 'activation_alpha' not in self.params[i] else self.params[i]['activation_alpha'])
+
+                    if epoch == 0:
+                        self.last_grad = grad
+
+                    layer_input = outputs[i-1] if i > 0 else input
+
+                    self.weights[i] -= grad @ layer_input.T * learn_rate
+                    self.biases[i] -= grad * learn_rate
+
+                    error = self.weights[i].T @ error
+
+            epoch_avg_loss = epoch_losses / len(X) 
+            total_losses.append(epoch_avg_loss)
+
+            print(f'Epoch: #{epoch+1}/{epochs} \n Loss: {epoch_avg_loss}')    
 
         if plot:
             plt.title('Loss Rate')
             plt.xlabel('Epochs')
             plt.ylabel('Loss')
             plt.grid()
-            plt.plot(costs_arr)
+            plt.plot(total_losses)
             plt.show()
 
     def save(self, filename):
-        np.save(filename, [self.W, self.b])
-        print('Model Saved')
+        np.save(filename, [self.weights, self.biases])
+        print('Model params were saved!')
 
     def load(self, filename):
         data = np.load(f'{filename}.npy', allow_pickle=True)
-        self.W = data[0]
-        self.b = data[1]
-        print('Paramas were loaded')
+        self.weights = data[0]
+        self.biases = data[1]
+        print('Paramas were loaded!')
